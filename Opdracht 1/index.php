ee178210f985a034afaf94cff689f6ae30326cbc
melkjuh <logansmit804@gmail.com>
2025-12-06 17:41:15 +0100

    Opdracht

---
+++ Opdracht 1/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<h1>Hoi Barry</h1>";
+echo "Ik zit op het ";
+
+$schoolnaam = "Techniek College Rotterdam ! :DDD";
+echo "$schoolnaam";
+
+$koekjes = "<br><3 koekjes <3";
+echo "$koekjes";
+
+// hoi barry in commentaar :P
+echo "<br>Groetjes van Logan";
+?>
